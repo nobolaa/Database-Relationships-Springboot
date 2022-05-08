87a287acf78c77695b00c9a91008daa86e43582c
nobolaa <nabilaliasserhaou@gmail.com>
2022-05-08 20:03:05 +0200

    Created new java project with javainitializr with dependencies:     - Spring Web     - Spring Data JPA     - MySQL Driver     - Lombok

---
+++ src/main/java/com/example/relationshipsSpringboot/RelationshipsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.relationshipsSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationshipsSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/relationshipsSpringboot/RelationshipsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relationshipsSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
